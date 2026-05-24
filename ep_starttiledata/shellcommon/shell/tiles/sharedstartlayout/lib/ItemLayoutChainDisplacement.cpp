4a0f89af685cba51decbf50e1934af68cfda5e02
AllieTheFox <AllieIsTyping@gmail.com>
2026-05-24 07:12:26 -0600

    feat(CItemLayoutChainDisplacement): Implement RuntimeClassInitialize

--- ep_starttiledata/shellcommon/shell/tiles/sharedstartlayout/lib/ItemLayoutChainDisplacement.cpp
+++ ep_starttiledata/shellcommon/shell/tiles/sharedstartlayout/lib/ItemLayoutChainDisplacement.cpp
@@ -2,3 +2,18 @@
 
 #include "ItemLayoutChainDisplacement.h"
 
+using namespace Microsoft::WRL;
+
+HRESULT CItemLayoutChainDisplacement::RuntimeClassInitialize(
+    CSimpleHashTable<UINT, RefCountedDirectionArray>* directionPrioritySets,
+    int deltaOfPriorityLengthOverShortestDistance, CHAIN_DISPLACEMENT_OPTION_FLAGS options)
+{
+    directionPrioritySets->Enum([this](UINT key, RefCountedDirectionArray value) -> bool const
+    {
+        m_directionPrioritySets.AddItem(key, value);
+        return true;
+    });
+    m_deltaOfPriorityLengthOverShortestDistance = deltaOfPriorityLengthOverShortestDistance;
+    m_options = options;
+    return S_OK;
+}
