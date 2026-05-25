cd97c87b875bb8c74188304da1a9630dc80519c2
Amrsatrio <20662640+Amrsatrio@users.noreply.github.com>
2026-05-26 03:22:50 +0700

    feat: impl CPortraitTileCellAssignor

--- ep_starttiledata/shellcommon/shell/tiles/sharedstartlayout/lib/PortraitTileCellAssignor.cpp
+++ ep_starttiledata/shellcommon/shell/tiles/sharedstartlayout/lib/PortraitTileCellAssignor.cpp
@@ -12,20 +12,41 @@ HRESULT CPortraitTileCellAssignor::CalculateLocationForNewItem(
     const Geometry::CSize& sizeItemCells, Geometry::CRect* rcNewBounds)
 {
     Geometry::CRect rcMaxBounds = _spCellArrayManager->GetCurrentCellArrayBounds();
-    int yStart;
-    if (rcMaxBounds.bottom - sizeItemCells.cy > rcMaxBounds.top)
-    {
-        yStart = rcMaxBounds.bottom - sizeItemCells.cy;
-    }
-    else
-    {
-        yStart = rcMaxBounds.top;
-    }
+    int yStart = max(rcMaxBounds.bottom - sizeItemCells.cy, rcMaxBounds.top);
     return _CalculateLocationForNewItem(yStart, sizeItemCells, rcNewBounds);
 }
 
-HRESULT CPortraitTileCellAssignor::_CalculateLocationForNewItem(int yStart, const Geometry::CSize& sizeItemCells, Geometry::CRect* rcNewBounds)
+HRESULT CPortraitTileCellAssignor::_CalculateLocationForNewItem(
+    int yStart, const Geometry::CSize& sizeItemCells, Geometry::CRect* rcNewBounds)
 {
-    static_assert(false, "Please implement me first!");
-    return S_OK;
+    HRESULT hr = E_FAIL;
+
+    Geometry::CSize sizeMaximumCellArray = _spCellArrayManager->GetMaximumCellArrayDimensions();
+    Geometry::CRect rcMaxBounds = _spCellArrayManager->GetCurrentCellArrayBounds();
+    for (int nYIndex = yStart; nYIndex < rcMaxBounds.bottom && FAILED(hr); ++nYIndex)
+    {
+        for (int nXIndex = rcMaxBounds.left; nXIndex < sizeMaximumCellArray.cx - sizeItemCells.cx - rcMaxBounds.left + 1 && FAILED(hr); ++nXIndex)
+        {
+            Geometry::CRect rcPotentialTileSpot(
+                nXIndex, nYIndex,
+                nXIndex + sizeItemCells.cx, nYIndex + sizeItemCells.cy);
+            if (_spCellArrayManager->IsValidRect(rcPotentialTileSpot)
+                && _spCellArrayManager->IsRectEmpty(rcPotentialTileSpot))
+            {
+                hr = S_OK;
+                *rcNewBounds = rcPotentialTileSpot;
+            }
+        }
+    }
+
+    if (FAILED(hr))
+    {
+        rcNewBounds->left = rcMaxBounds.left;
+        rcNewBounds->top = rcMaxBounds.bottom;
+        rcNewBounds->right = sizeItemCells.cx;
+        rcNewBounds->bottom = rcNewBounds->top + sizeItemCells.cy;
+        hr = S_OK;
+    }
+
+    return hr;
 }
