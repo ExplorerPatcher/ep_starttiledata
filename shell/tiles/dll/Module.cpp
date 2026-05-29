fc137d7049b89e02cb63202e548a7c472c3d471d
Amrsatrio <20662640+Amrsatrio@users.noreply.github.com>
2026-05-30 05:03:51 +0700

    feat: configure exported functions

---
+++ shell/tiles/dll/Module.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+
+using namespace Microsoft::WRL;
+
+extern "C" HRESULT WINAPI DllCanUnloadNow()
+{
+    return Module<InProc>::GetModule().Terminate();
+}
+
+extern "C" HRESULT WINAPI DllGetClassObject(REFCLSID objectClsid, REFIID requestedInterface, LPVOID* returnedInterface)
+{
+    return Module<InProc>::GetModule().GetClassObject(objectClsid, requestedInterface, returnedInterface);
+}
+
+BOOL APIENTRY DllMain(HMODULE instance, DWORD reason, LPVOID)
+{
+    switch (reason)
+    {
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls(instance);
+            break;
+    }
+    return TRUE;
+}
